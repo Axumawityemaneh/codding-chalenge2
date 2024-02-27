0cff9e94e194849171865530e425170946166280
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-02-27 20:39:06 +0300

    added sum numbers

---
+++ alem.py
@@ -0,0 +1,19 @@
+#take input from the user
+num1=8
+num2=6
+# Addition
+sum= num1 + num2
+print(sum)
+#subtraction
+sub= num1 - num2
+print(sub)
+#multiplication
+mut= num1 * num2
+print(mut)
+#divied
+div= num1 / num2
+print(div)
+
+
+
+
